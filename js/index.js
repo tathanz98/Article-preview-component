4fee20d4b638fac306111716168aa713953cc1c6
tathanz98 <sneiderz865@gmail.com>
2025-01-02 19:18:52 -0500

    El proyecto se esta subiendo

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function toggleShareOptions() {
+  const shareOptions = document.getElementById("shareOptions");
+  // Alternar la visibilidad de las opciones de compartir
+  shareOptions.style.display = (shareOptions.style.display === "block") ? "none" : "block";
+}
+
